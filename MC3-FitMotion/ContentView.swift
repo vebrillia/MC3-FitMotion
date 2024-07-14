894c688dbecd3e9f6298f3333bf34ffe450ebb05
Citta Catherine <cittacat@gmail.com>
2024-07-14 15:07:07 +0700

    Initial Commit

---
+++ MC3-FitMotion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MC3-FitMotion
+//
+//  Created by Citta Catherine Gozali on 14/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MC3-FitMotion/MC3_FitMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC3_FitMotionApp.swift
+//  MC3-FitMotion
+//
+//  Created by Citta Catherine Gozali on 14/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MC3_FitMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
