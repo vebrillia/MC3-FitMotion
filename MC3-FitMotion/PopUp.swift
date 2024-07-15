3102510aedba0106921cd8a4bdcad91a7b1f995c
cittacatherine <162044210+cittacatherine@users.noreply.github.com>
2024-07-15 17:27:54 +0700

    Stack Alert on top of Camera View through Content View

--- MC3-FitMotion/PopUp.swift
+++ MC3-FitMotion/PopUp.swift
@@ -11,46 +11,51 @@ struct PopUpGuide: ViewModifier {
     @Binding var isPresented: Bool
     
     @State private var offset: CGSize = .zero
+    @State private var selection = 0
     
     func body(content: Content) -> some View {
         ZStack {
             content
+                .blur(radius: isPresented ? 10 : 0)
             
             if isPresented {
                 VStack {
                     Spacer()
                     VStack {
-                        TabView {
-                            Text("Hello").tag(0)
-                            Text("You").tag(1)
-                        }
-                    .tabViewStyle(PageTabViewStyle())
-                    .frame(height: 200)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(10)
-                    .shadow(radius: 10)
-                    .offset(y: offset.height)
-                    .gesture(
-                        DragGesture()
-                            .onChanged { gesture in
-                                offset = gesture.translation}
-                            .onEnded { _ in
-                                if offset.height > 100 {
-                                    withAnimation {
-                                        isPresented = false
-                                        offset = .zero
+                        TabView(selection: $selection) {
+                            Text("Hello")
+                                .tag(0)
+                                .foregroundColor(.black)
+                            Text("You")
+                                .tag(1)
+                                .foregroundColor(.black)
+                        }                    .tabViewStyle(PageTabViewStyle(indexDisplayMode:  .always))
+                            .frame(width:300,height: 200)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .offset(y: offset.height)
+                            .gesture(
+                                DragGesture()
+                                    .onChanged { gesture in
+                                        offset = gesture.translation}
+                                    .onEnded { _ in
+                                        if offset.height > 100 {
+                                            withAnimation {
+                                                isPresented = false
+                                                offset = .zero
+                                            }
+                                        } else {
+                                            withAnimation {
+                                                offset = .zero
+                                            }
+                                        }
                                     }
-                                } else {
-                                    withAnimation {
-                                        offset = .zero
-                                    }
-                                }
-                            }
-                    )
-                    .transition(.move(edge: .bottom))
-                    .animation(.spring())
-                }
+                            )
+                            .transition(.move(edge: .bottom))
+                            .animation(.spring())
+                    }
                     Spacer().frame(height: 100)
                     
                 }
@@ -60,8 +65,24 @@ struct PopUpGuide: ViewModifier {
 }
 
 extension View {
-        func swipableAlert(isPresented: Binding<Bool>) -> some View {self.modifier(PopUpGuide(isPresented: isPresented))
-        }
+    func swipableAlert(isPresented: Binding<Bool>) -> some View {self.modifier(PopUpGuide(isPresented: isPresented))
     }
+}
 
 
+struct PopUpGuide_Previews: PreviewProvider {
+    @State static var isPresented = true
+    
+    static var previews: some View {
+        Text("Hello, World!")
+            .swipableAlert(isPresented: $isPresented)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
+
+//    .onAppear {
+//        showAlert = true
+//    }
+//    .edgesIgnoringSafeArea(.all)
+//} .swipableAlert(isPresented: $showAlert)
