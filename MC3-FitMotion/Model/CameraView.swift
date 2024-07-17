a80d65eaf201eb48bfafff9536d678e673c7d6c8
Christian <christian.kartika1@gmail.com>
2024-07-17 18:18:47 +0700

    MVVM

---
+++ MC3-FitMotion/Model/CameraView.swift
@@ -0,0 +1,13 @@
+
+import SwiftUI
+
+struct CameraView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> CameraViewController {
+        return CameraViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: CameraViewController, context: Context) {
+        // Update the view controller if needed
+    }
+    
+}
---
+++ MC3-FitMotion/Model/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MC3-FitMotion
+//
+//  Created by Citta Catherine Gozali on 14/07/24.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        CameraView()
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+#Preview {
+    ContentView()
+}
