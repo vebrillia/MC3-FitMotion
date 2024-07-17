253659b49a293d17997f86542b9c2c391c639d07
cittacatherine <162044210+cittacatherine@users.noreply.github.com>
2024-07-17 14:20:48 +0700

    Initiate Overlay View untuk Counter Set Rep

--- MC3-FitMotion/ContentView.swift
+++ MC3-FitMotion/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     @State private var showAlert = false
     
     var body: some View {
+<<<<<<< Updated upstream
         switch vm.dataScannerAccessStatus {
         case .notDetermined:
             Text("Requesting camera access")
@@ -31,11 +32,33 @@ struct ContentView: View {
                 .onAppear {
                     showAlert = true
                 }
+=======
+        ZStack{
+            OverlaySetRepCounter()
+        pulseIndicator()
+>>>>>>> Stashed changes
         }
+           
+//        switch vm.dataScannerAccessStatus {
+//        case .notDetermined:
+//            Text("Requesting camera access")
+//        case .cameraAccessNotGranted:
+//            Text("Please provide access to the camera in settings")
+//        case .cameraNotAvailable:
+//            Text("Your device doesn't have a camera")
+//        case .scannerNotAvailable:
+//            Text("Your device doesn't support scanning barcode")
+//        case .scannerAvailable:
+//            CameraView()
+//                .edgesIgnoringSafeArea(.all)
+//                .onAppear {
+//                    showAlert = true
+//                }
+//        }
         
     }
 }
 
-//#Preview {
-//    ContentView()
-//}
+#Preview {
+    ContentView()
+}
