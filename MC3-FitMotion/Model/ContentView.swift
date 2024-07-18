94b75602cc1d1e633275dfb944ee8387155e735f
vebrillia <94434266+vebrillia@users.noreply.github.com>
2024-07-18 16:38:46 +0700

    add counter

--- MC3-FitMotion/Model/ContentView.swift
+++ MC3-FitMotion/Model/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State var cameraViewModel = CameraViewModel()
+    @State private var counter = 0
     
     var body: some View {
         ZStack {
@@ -20,6 +21,7 @@ struct ContentView: View {
                 VStack {
                     Text(cameraViewModel.label)
                     Text("\(cameraViewModel.confidence)")
+                    Text("Counter: \(counter)")
                 }.padding()
                     .background(.yellow)
             }
@@ -28,6 +30,11 @@ struct ContentView: View {
         }
         .background(.purple)
         .ignoresSafeArea()
+        .onChange(of: cameraViewModel.label) { newLabel in
+            if newLabel == "Benar" {
+                counter += 1
+            }
+        }
     }
 }
 
