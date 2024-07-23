ec2c9c3c23f9eb882b5119f584d3bc8a6faff8d6
vebrillia <94434266+vebrillia@users.noreply.github.com>
2024-07-23 15:51:36 +0700

    Editing ContentView

--- MC3-FitMotion/Model/ContentView.swift
+++ MC3-FitMotion/Model/ContentView.swift
@@ -4,12 +4,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var isPopupPresented = true
     var body: some View {
-       
-       CameraPositionView()
-//        ZStack{
-//            WorkoutsListView()
-//        }
-//        .swipableAlert(isPresented: $isPopupPresented)
+            WorkoutsListView()
     }
         
 }
