2d7a96ecda3fd7fef16169d7895ae6fb732da148
Christian <christian.kartika1@gmail.com>
2024-07-23 18:24:40 +0700

    ssss

---
+++ MC3-FitRep/Model/WorkoutListView.swift
@@ -0,0 +1,24 @@
+
+import SwiftUI
+
+struct WorkoutsListView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                NavigationBar(title: "Workouts", subtitle: "Build your muscles!", image: "info.circle")
+                
+                WorkoutList(title: "Bicep Curl", subtitle: "Arms", image: "BicepPerson", destination: AnyView(PreWorkoutView()))
+                WorkoutListLocked(title: "Push Up", subtitle: "Arms & Chest")
+                WorkoutListLocked(title: "Squat", subtitle: "Thighs")
+                Spacer()
+            }
+            .background(Color.custWhite)
+            .navigationTitle("Workout List")
+            .toolbar(.hidden)
+        }
+    }
+}
+
+#Preview {
+    WorkoutsListView()
+}
