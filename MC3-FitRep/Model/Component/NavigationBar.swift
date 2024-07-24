f778e37f12c67ffaccb87975a716e6169fdab672
Christian <christian.kartika1@gmail.com>
2024-07-24 15:10:09 +0700

    add gif

--- MC3-FitRep/Model/Component/NavigationBar.swift
+++ MC3-FitRep/Model/Component/NavigationBar.swift
@@ -4,18 +4,23 @@ import SwiftUI
 struct NavigationBar: View {
     private let title: String
     private let subtitle: String
-    private let image: String
+    private let rightButton: AnyView?
+    private let leftButton: AnyView?
     
     @State private var isShowingPopover = false
     
-    init(title: String, subtitle: String, image: String) {
+    init(title: String, subtitle: String) {
         self.title = title
         self.subtitle = subtitle
-        self.image = image
     }
     
     var body: some View {
         VStack {
+            
+            HStack {
+                
+            }
+            
             HStack {
                 Text(title)
                     .frame(height: 32)
@@ -23,25 +28,6 @@ struct NavigationBar: View {
                     .font(.largeTitle)
                     .bold()
                 Spacer()
-                
-                Button(action: {
-                    self.isShowingPopover.toggle()
-                }) {
-                    Image(systemName: image)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
-                        .foregroundStyle(Color.custTosca)
-                }
-                .popover(isPresented: $isShowingPopover, arrowEdge: .top, content: {
-                    Text("Jumlah set dalam workout berkisar antara 3-6 set\nRepetisi dalam workout tergantung dari target yang diinginkan dan berkaitan dengan kemampuan dalam mengangkat beban.\n\nStrength : <5 Rep\nMuscle Growth : 5-15 Rep\nMuscle Tone : >15 Rep\n\nContoh, jka ingin mengambil jumlah repetisi dibawah 5, maka carilah beban yang memang tidak bisa diangkat melebihi 5 repetisi. Begitu juga untuk jumlah repetisi lainnya. Jika beban yang diangkat lebih ringan dari kemampuan yang ada, tidak akan terjadi pertumbuhan pada otot.")
-                        .font(.caption)
-                        .padding(.horizontal)
-                        .frame(height: 260)
-                        .presentationCompactAdaptation(.popover)
-                        .presentationBackgroundInteraction(.enabled)
-                })
-                .padding(.trailing)
             }
     
             HStack {
