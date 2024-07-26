569dc213782b1052e6298899018da995cb1a05c6
Christian <christian.kartika1@gmail.com>
2024-07-26 17:49:38 +0700

    UI

--- MC3-FitRep/View/Component/NavigationBar.swift
+++ MC3-FitRep/View/Component/NavigationBar.swift
@@ -5,16 +5,6 @@ struct NavigationBar: View {
     let title: String
     let subtitle: String
     
-    @State private var isShowingPopover = false
-    
-    var rightItem: AnyView = AnyView(EmptyView())
-    
-    init(title: String, subtitle: String, rightItem: AnyView) {
-        self.title = title
-        self.subtitle = subtitle
-        self.rightItem = rightItem
-    }
-    
     init(title: String, subtitle: String) {
         self.title = title
         self.subtitle = subtitle
@@ -29,17 +19,15 @@ struct NavigationBar: View {
                     .foregroundStyle(Color.custBlack)
                     .font(.largeTitle)
                     .bold()
+                
                 Spacer()
-                rightItem
             }
-    
-            if !subtitle.isEmpty {
-                HStack {
-                    Text(subtitle)
-                        .foregroundStyle(Color.custGray)
-                        .frame(height: 8)
-                    Spacer()
-                }
+            
+            HStack {
+                Text(subtitle)
+                    .foregroundStyle(Color.custGray)
+                    .frame(height: 8)
+                Spacer()
             }
         }
         .padding()
--- MC3-FitRep/View/Component/SetRepInfoButton.swift
+++ MC3-FitRep/View/Component/SetRepInfoButton.swift
@@ -1,40 +1,40 @@
+////
+////  RepInfoButton.swift
+////  MC3-FitRep
+////
+////  Created by Christian on 24/07/24.
+////
 //
-//  RepInfoButton.swift
-//  MC3-FitRep
+//import SwiftUI
 //
-//  Created by Christian on 24/07/24.
+//struct SetRepInfoButton: View {
+//    @State private var isShowingPopover = false
+//    
+//    var body: some View {
+//        Button(action: {
+//            self.isShowingPopover.toggle()
+//        }) {
+//            Image(systemName: "info.circle")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: 20, height: 20)
+//                .foregroundStyle(Color.custTosca)
+//        }
+//        .popover(isPresented: $isShowingPopover, arrowEdge: .top, content: {
+//            ScrollView {
+//                JustifiedTextView(text: "Jumlah set dalam workout berkisar antara 3-6 set. Repetisi dalam workout tergantung dari target yang diinginkan dan berkaitan dengan kemampuan dalam mengangkat beban.\n\nStrength : <5 Rep\nMuscle Growth : 5-15 Rep\nMuscle Tone : >15 Rep\n\nContoh, jika ingin mengambil jumlah repetisi dibawah 5, maka carilah beban yang memang tidak bisa diangkat melebihi 5 repetisi. Begitu juga untuk jumlah repetisi lainnya. Jika beban yang diangkat lebih ringan dari kemampuan yang ada, tidak akan terjadi pertumbuhan pada otot.")
+//                    .font(.caption)
+//                    .padding(.horizontal)
+//                    .frame(width: 320, height: 240)
+//                    .presentationCompactAdaptation(.popover)
+//                    .presentationBackgroundInteraction(.enabled)
+//            }
+//            
+//        })
+//        .padding(.trailing)
+//    }
+//}
 //
-
-import SwiftUI
-
-struct SetRepInfoButton: View {
-    @State private var isShowingPopover = false
-    
-    var body: some View {
-        Button(action: {
-            self.isShowingPopover.toggle()
-        }) {
-            Image(systemName: "info.circle")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 20, height: 20)
-                .foregroundStyle(Color.custTosca)
-        }
-        .popover(isPresented: $isShowingPopover, arrowEdge: .top, content: {
-            ScrollView {
-                JustifiedTextView(text: "Jumlah set dalam workout berkisar antara 3-6 set. Repetisi dalam workout tergantung dari target yang diinginkan dan berkaitan dengan kemampuan dalam mengangkat beban.\n\nStrength : <5 Rep\nMuscle Growth : 5-15 Rep\nMuscle Tone : >15 Rep\n\nContoh, jika ingin mengambil jumlah repetisi dibawah 5, maka carilah beban yang memang tidak bisa diangkat melebihi 5 repetisi. Begitu juga untuk jumlah repetisi lainnya. Jika beban yang diangkat lebih ringan dari kemampuan yang ada, tidak akan terjadi pertumbuhan pada otot.")
-                    .font(.caption)
-                    .padding(.horizontal)
-                    .frame(width: 320, height: 240)
-                    .presentationCompactAdaptation(.popover)
-                    .presentationBackgroundInteraction(.enabled)
-            }
-            
-        })
-        .padding(.trailing)
-    }
-}
-
-#Preview {
-    SetRepInfoButton()
-}
+//#Preview {
+//    SetRepInfoButton()
+//}
